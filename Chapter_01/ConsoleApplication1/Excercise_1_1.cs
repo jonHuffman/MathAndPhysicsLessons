d98a6c819b0b29698cd6fa3f1e949179f6c4fcfe
Jon Huffman <jon@jonhuffman.com>
2018-03-27 22:30:43 -0400

    Implemented the conversion from a Number system to Decimal

--- Chapter_01/ConsoleApplication1/Excercise_1_1.cs
+++ Chapter_01/ConsoleApplication1/Excercise_1_1.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace ConsoleApplication1
 {
+    //----------------------------------------------
+    // References
+    //  - http://mathonweb.com/help_ebook/html/expolog_4.htm
     public class Excercise_1_1
     {
         public enum BaseSystem
@@ -30,34 +33,93 @@ namespace ConsoleApplication1
         /// Takes a number represented as an array of digits and converts it from one Base system to another.
         /// </summary>
         /// <param name="number">Digits should be ordered in the array from most to least significant. 724 would be [ 7, 2, 4] </param>
-        public static void ConvertBase(int[] number, BaseSystem frombase, BaseSystem toBase)
+        /// <remarks>This function can support any base system supported by <see cref="BaseSystem"/>, we assume that all Base systems above decimal make use of A-F characters</remarks>
+        public static void ConvertBase(string number, BaseSystem frombase, BaseSystem toBase)
         {
-
+            int decimalValue = ToDecimal(number, frombase);
         }
 
         /// <summary>
         /// Converts a number from any base system to Decimal.
         /// </summary>
         /// <param name="number">Digits should be ordered in the array from most to least significant. 724 would be [ 7, 2, 4]</param>
-        /// <returns>A list containing the digits of the number sorted from most to least significant.</returns>
-        private static List<int> ToDecimal(int[] number, BaseSystem frombase)
+        /// <returns>The number converted to decimal</returns>
+        /// <remarks>This function can support any base system supported by <see cref="BaseSystem"/>, we assume that all Base systems above decimal make use of A-F characters</remarks>
+        private static int ToDecimal(string number, BaseSystem frombase)
         {
-            // To ease the storage, number will be stored least to most
-            List<int> convertedNumber = new List<int>();
-
             // Reverse for ease of calculation
-            Array.Reverse(number);
+            char[] splitNumber = number.ToCharArray();
+            Array.Reverse(splitNumber);
 
-            // Base ^ Index x Digit = DecimalOfDigit
-            for(int i = 0; i < number.Length; i++)
+            // Confirm that we won't immediately overflow the int
+            int mostSignificantDigit = Parse(splitNumber[splitNumber.Length - 1], frombase);
+            int largestSupportedIndex = (int)(Math.Log10(int.MaxValue / mostSignificantDigit) / Math.Log10((int)frombase));
+
+            if (splitNumber.Length - 1 > largestSupportedIndex)
             {
-                // WHAT about overflowing an int for large numbers?
-                int decimalValue = (int)frombase ^ i * number[i];
-                decimalValue
+                throw new OverflowException(string.Format("A {0} number with the most significant digit being {1} cannot have more than {2} digits. You have {3} digits.",
+                    frombase,
+                    mostSignificantDigit,
+                    largestSupportedIndex + 1,
+                    splitNumber.Length));
             }
 
-            convertedNumber.Reverse();
-            return convertedNumber;
+            // Digit x Base ^ Index = DecimalOfDigit
+            int decimalConversion = 0;
+            for (int i = splitNumber.Length - 1; i >= 0; i--)
+            {
+                // WHAT about overflowing an int for large numbers?
+                int decimalNumber = Parse(splitNumber[i], frombase) * (int)Math.Pow((int)frombase, i);
+
+                if (decimalNumber > int.MaxValue - decimalConversion)
+                {
+                    throw new OverflowException("The number has overflowed the integer during decimal conversion.");
+                }
+
+                decimalConversion += decimalNumber;
+            }
+
+            return decimalConversion;
+        }
+
+        private static int Parse(char number, BaseSystem baseSystem)
+        {
+            int value = (int)char.GetNumericValue(number);
+            if (value >= 0 && value <= 9)
+            {
+                return value;
+            }
+            else
+            {
+                switch (number)
+                {
+                    case 'A':
+                    case 'a':
+                        return 10;
+                    case 'B':
+                    case 'b':
+                        return 11;
+                    case 'C':
+                    case 'c':
+                        return 12;
+                    case 'D':
+                    case 'd':
+                        return 13;
+                    case 'E':
+                    case 'e':
+                        return 14;
+                    case 'F':
+                    case 'f':
+                        return 15;
+                    default:
+                        throw new NotSupportedException(string.Format("Character {0} is not supported by this conversion tool.", number));
+                }
+            }
+        }
+
+        private static double NthRoot(double number, int power)
+        {
+            return Math.Pow(number, 1.0 / power);
         }
     }
 }
--- Chapter_01/ConsoleApplication1/Program.cs
+++ Chapter_01/ConsoleApplication1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Excercise_1_1.ConvertBase(2147483647.ToString(), Excercise_1_1.BaseSystem.Decimal, Excercise_1_1.BaseSystem.Binary);
         }
     }
 }
